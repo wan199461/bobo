7da91e53ec1bdc92c18a1c971ed7869654ed9a98
BW <hhwanbo@gmail.com>
2018-12-02 20:47:28 +0800

    commit

--- src/main/java/com/wanbo/rocketmq/Config.java
+++ src/main/java/com/wanbo/rocketmq/Config.java
@@ -4,11 +4,66 @@ import com.alibaba.fastjson.JSONObject;
 
 public class Config {
     
-    public static String data = "{\"reqData\":{\"phone\":\"846fbf4a43aa1e1cfb52962e1ea5cb81\"},\"institutionId\":\"jmeter_test\",\"requestId\":\"20170921164605547-8349BEEEE5C543AF\",\"responseData\":{\"matchScore\":1,\"multiScore\":7.0,\"matchTag\":1},\"prodList\":[\"MUL\"]}";
-
+    public static String data = "{\r\n" + 
+            "    \"requestId\": \"20170921164605547-8349BEEEE5C543AF\",\r\n" + 
+            "    \"responseData\": {\r\n" + 
+            "        \"creditScore\": 0.000002358,\r\n" + 
+            "        \"matchTag\": 1\r\n" + 
+            "    },\r\n" + 
+            "    \"sourceCode\": \"fintell\",\r\n" + 
+            "    \"apiCode\": \"FinTell_FinTell_C4CFP\"\r\n" + 
+            "}";
+    
+    
+    public static String data() {
+    	return generateEnumData().toJSONString();
+//    	return data;
+    }
+    
+    
+    public static JSONObject generateEnumData() {
+    	
+    	JSONObject obj = new JSONObject();
+    	obj.put("requestId", "20181126164605547-8349BEEEE5C543AF");   	
+	    	JSONObject responseData = new JSONObject();
+	    	long time = System.currentTimeMillis();
+	    	if(time%4 == 0) {
+	    		responseData.put("riskTag", "md000");
+	    	}else if(time%4 == 1) {
+	    		responseData.put("riskTag", "md010");
+	    	}else if(time%4 == 2) {
+	    		responseData.put("riskTag", "md020");
+	    	}else if(time%4 == 3) {
+	    		responseData.put("riskTag", "md030");
+	    	}
+//    	responseData.put("responseData", responseData);
+    	obj.put("responseData", responseData);
+    	obj.put("sourceCode", "fintell");
+    	obj.put("apiCode", "FinTell_FinTell_AMUL");
+    	return obj;
+    }
+    
+    
     public static void main(String...strings) {
-        JSONObject obj = JSONObject.parseObject(data);
-        System.out.println(obj.get("reqData"));
+//        JSONObject obj = JSONObject.parseObject(data);
+//        System.out.println(obj.getJSONObject("responseData").get("creditScore"));
+    	JSONObject obj = generateEnumData();
+    	System.out.println(obj.toJSONString());
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/wanbo/utils/App.java
@@ -0,0 +1,8 @@
+package com.wanbo.utils;
+
+public class App {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+}
