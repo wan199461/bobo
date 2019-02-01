03527856dd55a09e49eaf238a7a7f4fddfae3f5e
BW <hhwanbo@gmail.com>
2019-02-01 14:30:35 +0800

    commit changes

--- src/main/java/com/wanbo/App.java
+++ src/main/java/com/wanbo/App.java
@@ -7,72 +7,53 @@ import java.util.List;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws MyDefaultException
-    {
-    	/**
-    	 *  测试1
-    	 */
-        List<String> list=new ArrayList<String>();
+public class App {
+    public static void main(String[] args) throws MyDefaultException {
+        /**
+         * 测试1
+         */
+        List<String> list = new ArrayList<String>();
         addList(list);
         list.add("Lo");
-        for(String li:list) {
-        	System.out.println(li);
+        for (String li : list) {
+            System.out.println(li);
         }
-        
-        
-        
+
         /**
          * 测试2
          */
-        App a=new App();
-        List<String> list2=new ArrayList<String>();
+        App a = new App();
+        List<String> list2 = new ArrayList<String>();
         list2.add("Lo");
         a.addList2(list2);
-        for(String li:list2) {
-        	System.out.println(li);
+        for (String li : list2) {
+            System.out.println(li);
         }
-        
-        
+
         /**
          * 测试3
          */
-        DataBean bean=new DataBean();
-        bean.age=1;
-        bean.name="1";
-        bean.len=1;
-        App app3=new App();
+        DataBean bean = new DataBean();
+        bean.age = 1;
+        bean.name = "1";
+        bean.len = 1;
+        App app3 = new App();
         changeBean(bean);
-        System.out.println("Age:="+bean.age);
+        System.out.println("Age:=" + bean.age);
     }
-    
+
     public static void changeBean(DataBean bean) {
-    	bean.age=2;
-    	bean.name="2";
-    	bean.len=2;
+        bean.age = 2;
+        bean.name = "2";
+        bean.len = 2;
     }
-    
-    public  static void addList(List<String> list) {
-    	 list.add("HEl");
+
+    public static void addList(List<String> list) {
+        list.add("HEl");
     }
-    
+
     public void addList2(List<String> list) {
-    	 list.add("Li");
+        list.add("Li");
     }
-    
-    
-    
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/main/java/com/wanbo/App5.java
@@ -0,0 +1,16 @@
+package com.wanbo;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+import org.mortbay.util.ajax.WaitingContinuation;
+
+public class App5 {
+    
+    
+    
+}
