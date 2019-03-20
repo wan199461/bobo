93f6a11c6e3d13c3c3cba3d3f4df37a0af0f6813
BW <hhwanbo@gmail.com>
2019-03-20 16:37:43 +0800

    commit changes

--- src/main/java/com/wanbo/java8/stream/NoParamFuncClass.java
+++ src/main/java/com/wanbo/java8/stream/NoParamFuncClass.java
@@ -1,25 +1,39 @@
 package com.wanbo.java8.stream;
 
 public class NoParamFuncClass {
-  
-  public NoParamFuncClass() {
-    System.out.println("Construct method");
-  }
-  
-  public void print() {
-    System.err.println("NoParamFuncClass");
-  }
-  
-  public static void printStatic(){
-     System.out.println("Static method");
-  }
-  
-  public void printFunc() {
-    System.out.println("Func method");
-  }
-  
-  public void func1(OneParamFuncInterface np) {
-      np.noParam();
-  }
+
+    public NoParamFuncClass(OneParamFuncInterface of) {
+        of.noParam();
+    }
+
+    public void print() {
+        System.err.println("NoParamFuncClass");
+    }
+
+    public static void printStatic(OneParamFuncInterface of) {
+        of.noParam();
+    }
+
+    public void printFunc() {
+        System.out.println("Func method");
+    }
+
+    public void func1(OneParamFuncInterface np) {
+        np.noParam();
+    }
+
+    public static void main(String[] args) {
+        printStatic(() -> {
+            System.out.println("hehe");
+        });
+        
+        
+        NoParamFuncClass nc = new NoParamFuncClass(()->{
+            System.out.println("init method");
+        });
+        nc.func1(()-> {
+            System.out.println("Func Data");
+        });
+    }
 
 }
