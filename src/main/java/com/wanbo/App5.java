05f86dad10b6baf2cd1ab43597990780abf9a38c
BW <hhwanbo@gmail.com>
2019-04-21 15:54:25 +0800

    commit changes

--- src/main/java/com/wanbo/App5.java
+++ src/main/java/com/wanbo/App5.java
@@ -1,24 +1,25 @@
 package com.wanbo;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import com.alibaba.fastjson.JSON;
+//import com.bw.cipher.RedisLoader;
 
 public class App5 {
- 
-   private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-HH mm:ss.SSS");
-    
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-HH mm:ss.SSS");
+
     public static void main(String[] args) {
-          System.out.println("begin args= " + JSON.toJSONString(args));
-          App5 app = new App5();
-          for(int i=0; i <1;) {
-              app.waiting();
-              System.err.println("System Wait " + sdf.format(new Date()));
-          }
+        /*
+         * System.out.println("begin args= " + JSON.toJSONString(args)); App5 app = new
+         * App5(); for(int i=0; i <1;) { app.waiting();
+         * System.err.println("System Wait " + sdf.format(new Date())); }
+         */
+/*
+        RedisLoader rloader = new RedisLoader();
+        System.out.println(rloader.redisSet("AFG", "中华人民共和国万岁"));
+        System.out.println(rloader.redisGet("AFG"));*/
     }
-    
-    
+
     private synchronized void waiting() {
         try {
             this.wait(1000);
